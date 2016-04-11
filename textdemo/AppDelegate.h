cfe31e28dfe0a42c4480c4ea338036f002560a0e
langker <langker@aliyun.com>
2016-04-11 14:38:43 +0800

    Initial Commit

---
+++ textdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textdemo
+//
+//  Created by langker on 16/4/11.
+//  Copyright © 2016年 langker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textdemo
+//
+//  Created by langker on 16/4/11.
+//  Copyright © 2016年 langker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
